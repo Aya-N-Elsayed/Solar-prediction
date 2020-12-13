75a3137de276c789327f5657a20d4d2112bdca94
Ayatelrahman Elsayed <aeit.el.rahman.nasser.k@gmail.com>
2020-12-12 19:09:27 -0700

    updating figures

--- Hyperparameters/boxplot_hyper.py
+++ Hyperparameters/boxplot_hyper.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Creating plot plt.boxplot
 data = pd.read_csv("svrS_hyper.csv")
-data.boxplot(column=['R2','RMSE','MAE'])
+data.boxplot(column=['R2','RMSE','MAE', 'C','kernel'])
 plt.title('SVR hyperparamter tuning results')
 plt.xlabel('Metrics')
 plt.show()
@@ -18,7 +18,7 @@ plt.show()
 
 # Creating plot plt.boxplot
 data = pd.read_csv("rfL_hyper.csv")
-data.boxplot(column=['R2','RMSE','MAE'])
+data.boxplot(column=['R2','RMSE','MAE','n_est'])
 plt.title('RF hyperparamter tuning results for whole data')
 plt.xlabel('Metrics')
 plt.show()
